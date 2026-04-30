dfe5b2471eb595ad96f68aef8302166e3f09875e
igor <igorc7871@gmail.com>
2026-04-30 14:39:55 -0300

    chore: Pizzaria Controller

---
+++ ProjetoPizzaria/Controllers/PIzzariaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoPizzaria.Models;
+
+namespace ProjetoPizarriaOne.Controllers
+{
+    public class PizzariaController : Controller
+    {
+        public IActionResult NovoPedido()
+        {
+            return View();
+        }
+        public IActionResult NotaPedido(Pedido pedido)
+        {
+            ViewBag.total = pedido.CalcularPedido();
+            pedido.horarioPedido = DateTime.Now;
+            return View(pedido);
+        }
+    }
+}
